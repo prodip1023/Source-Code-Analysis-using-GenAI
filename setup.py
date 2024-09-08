961779d0afb83b846fce7675d759035d74bc2e9c
“prodip1023” <prodip1023@gmail.com>
2024-09-09 00:33:00 +0530

    add trials file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Prodip Sarkar',
+    author_email= 'prodip1023@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
